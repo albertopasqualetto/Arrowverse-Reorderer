8b1105bf4d29b3f9cd226c6383c7918571106dec
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2022-06-27 00:39:50 +0200

    Added main and args parsing

--- arrowverse_reorderer.py
+++ arrowverse_reorderer.py
@@ -7,7 +7,6 @@ get all filenames
 move them to another directory and rename them by prepending "#" column to old filename getting it from the line Series and Title
 """
 
-import sys
 import os
 import json
 import requests
@@ -16,31 +15,33 @@ import glob
 import filetype
 import shutil
 
-def reorder(folder):
-	nameDestFolder="reordered"
-	destFolder= os.path.join(folder,nameDestFolder)
-	if not os.path.exists(destFolder):
-		os.makedirs(destFolder)
+def reorder(folders, name_dest_folder, destination_path, dry_run=False):
+	# set and create destination folder
+	if not os.path.exists(destination_path):
+		os.makedirs(destination_path)
 
+	# get Arrowverse order from website
 	url= "https://arrowverse.info"
 	df= pd.read_html(requests.get(url).content)[-1]
-
 	del df["Original Air Date"]
 	del df["Source"]
-	
-	# for each file in directory, move to new directory
-	for file in glob.glob(os.path.join(folder,"*[!"+nameDestFolder+"]","*"), recursive=True):
-		# if file is a video
-		if (filetype.is_video(file) and len(os.path.basename(file).split(" - "))==3):	# if file is a video and its name is in the format * - * - *
-			series= os.path.basename(file).split(" - ")[0]
-			episode= os.path.basename(file).split(" - ")[1]
-			end= os.path.basename(file).split(" - ")[2]
-			
-			rowDf= df.loc[(df['Series'] == series) & (df['Episode'] == episode)]
-			row= json.loads(rowDf.to_json(orient="records"))[0]
-			number= str(row['#']).zfill(3)
 
-			shutil.move(file, os.path.join(destFolder, number+" - "+series+" - "+episode+" - "+end))
+	# for each file in directories, move to new directory
+	for folder in folders:
+		for file in glob.glob(os.path.join(folder,"*[!"+name_dest_folder+"]","*"), recursive=True):
+			# if file is a video
+			if (filetype.is_video(file) and len(os.path.basename(file).split(" - "))==3):	# if file is a video and its name is in the format * - * - *
+				series= os.path.basename(file).split(" - ")[0]
+				episode= os.path.basename(file).split(" - ")[1]
+				end= os.path.basename(file).split(" - ")[2]
+				
+				row_df= df.loc[(df['Series'] == series) & (df['Episode'] == episode)]
+				row= json.loads(row_df.to_json(orient="records"))[0]
+				number= str(row['#']).zfill(3)
+
+				if not dry_run:
+					shutil.move(file, os.path.join(destination_path, number+" - "+series+" - "+episode+" - "+end))
+				print(file+" moved")
 
 
 
